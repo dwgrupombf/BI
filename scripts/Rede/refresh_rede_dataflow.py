01cc145b8cd3662529068a1a4a5ff99ed5b16a1d
Erica Rafael <ericasrafael@gmail.com>
2026-05-13 13:31:07 -0300

    dfc

--- scripts/Rede/refresh_rede_dataflow.py
+++ scripts/Rede/refresh_rede_dataflow.py
@@ -78,7 +78,8 @@ def get_workspace_id(headers):
 def refresh_dataflow(headers, workspace_id, dataflow_name, payload):
     r = requests.get(
         BASE_URL + f"groups/{workspace_id}/dataflows", headers=headers
-    ) r.raise_for_status()
+    )
+    r.raise_for_status()
 
     for df in r.json()["value"]:
         if df["name"] == dataflow_name:
